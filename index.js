931a34bfc3eb62d421541d0124b62b590ae48116
Asadullah246 <asadm2258@gmail.com>
2022-05-07 01:47:59 +0600

    about done solving errors

--- index.js
+++ index.js
@@ -6,9 +6,8 @@ require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
-
-
 // middleware 
+
 app.use(cors());
 app.use(express.json());
 
